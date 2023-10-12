fb83041af19980f0ddf0d3068354d969224c46ec
tamigld <thami.galves@outlook.com>
2023-10-11 21:23:07 -0300

    header finalizado

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+
+import {Route, Routes, BrowserRouter} from 'react-router-dom'
+import Homepage from './pages/home/index.tsx'
+import Header from './components/header/index.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path='/' element={<Homepage/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,7 @@
+import './home.css'
+
+export default function Homepage(){
+    return(
+        <div>ihu</div>
+    )
+}
